09dab76379da282dccfa6f17b08ed326220aa5ae
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-02-28 13:31:06 -0400

    orderBy

---
+++ src/ejercicios/crud.md
@@ -0,0 +1,75 @@
+# Referencia al documento
+const usuariosRef = db.collection("usuarios");
+
+## objeto
+db.collection("usuarios")
+    .add(usuario)
+    .then(docRef => {
+        console.log (docRef)
+    })
+
+
+ # insert into usuarios...
+
+ db.collection("usuarios")
+    .add(usuario)
+    .then(docRef => {
+        console.log (docRef)
+    })
+    .catch (e => console.log ("error", e))
+Destructivo
+usuariosRef
+.doc("cHuJKc9AIt3Tk30Br6hN")
+.set ({
+    activo:true,
+    edad:18,
+    casado:true,
+});
+
+usuariosRef
+.doc("cHuJKc9AIt3Tk30Br6hN")
+.delete()
+.then(() => console.log ("borrado") )
+.catch (e => console.log ("error", e))
+
+select * from usuarios:
+usuariosRef
+.onSnapshot( retornaDocumentos)
+
+usuariosRef.get ().then (retornaDocumentos);    
+
+
+select * from usuarios 
+where activo = true
+
+
+usuariosRef.where("activo", "==", true).get ().then (retornaDocumentos);    
+
+
+select * from usuarios 
+where salario > 1800
+
+
+usuariosRef.where("salario", "==", 1800)
+.get ().then (retornaDocumentos);    
+
+
+select * from usuarios 
+where salario > 1800 and salario < 2300 
+
+
+usuariosRef.where("salario", "==", 1800)
+.where("salario", "<", 2300)
+.get ().then (retornaDocumentos);  
+
+
+select * from usuarios 
+where salario > 1800 
+and salario == true
+
+
+usuariosRef.where("salario", "==", 1800)
+.where("activo","==", true)
+.get ().then (retornaDocumentos); 
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,8 @@
 import db from "./firebase/config"
 import { retornaDocumentos } from "./helpers/mostrar-documentos";
+import { firestore } from "firebase";
 
-
-const usuario ={
-
-    nombre: "Ramon",
-    activo: false,
-    fechanaci: 0,
-}
 const usuariosRef = db.collection("usuarios");
 
-//db.collection("usuarios")
- //   .add(usuario)
-   // .then(docRef => {
-     //   console.log (docRef)
-    //})
-   // .catch (e => console.log ("error", e))
-//Destructivo
-//usuariosRef
-//.doc("cHuJKc9AIt3Tk30Br6hN")
-//.set ({
-  //  activo:true,
-    //edad:18,
-    //casado:true,
-//});
-
-//usuariosRef
-//.doc("cHuJKc9AIt3Tk30Br6hN")
-//.delete()
-//.then(() => console.log ("borrado") )
-//.catch (e => console.log ("error", e))
-
-//select * from usuarios:
-//usuariosRef
-//.onSnapshot( retornaDocumentos)
-
-//usuariosRef.get ().then (retornaDocumentos);    
-
-/*
-select * from usuarios 
-where activo = true
-*/
-
-/*usuariosRef.where("activo", "==", true).get ().then (retornaDocumentos);    
-
-/*
-select * from usuarios 
-where salario > 1800
-*/
-
-/*usuariosRef.where("salario", "==", 1800)
-.get ().then (retornaDocumentos);    
-
-/*
-select * from usuarios 
-where salario > 1800 and salario < 2300 
-*/
-
-//usuariosRef.where("salario", "==", 1800)
-//.where("salario", "<", 2300)
-//.get ().then (retornaDocumentos);  
-
-/*
-select * from usuarios 
-where salario > 1800 
-and salario == true
-*/
-
-usuariosRef.where("salario", "==", 1800)
-.where("activo","==", true)
-.get ().then (retornaDocumentos); 
-
-
+usuariosRef.orderBy("nombre","desc")
+.get().then(retornaDocumentos) 
