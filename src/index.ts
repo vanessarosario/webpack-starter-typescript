4aaf1c041798e36221aec795c58f88cea4ccaf73
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-02-28 00:28:05 -0400

    actualizar data en firestore

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,21 @@ const usuario ={
     activo: false,
     fechanaci: 0,
 }
+const usuariosRef = db.collection("usuarios");
 
-db.collection("usuarios")
-    .add(usuario)
-    .then(docRef => {
-        console.log (docRef)
-    })
-    .catch (e => console.log ("error", e))
+//db.collection("usuarios")
+ //   .add(usuario)
+   // .then(docRef => {
+     //   console.log (docRef)
+    //})
+   // .catch (e => console.log ("error", e))
+//Destructivo
+//usuariosRef
+//.doc("cHuJKc9AIt3Tk30Br6hN")
+//.set ({
+  //  activo:true,
+    //edad:18,
+    //casado:true,
+//});
+
+
